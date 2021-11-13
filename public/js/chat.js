a6a3dcec0eb7da81102694a78afa0a9597dc024f
sinsarul haq vm <sinsarulhaq.pro@gmail.com>
2021-11-13 16:29:39 +0530

    broadcast added

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,4 @@
-const e = require("cors")
+// const e = require("cors")
 
  const socket = io()
 
@@ -12,7 +12,7 @@ const e = require("cors")
 // })
 document.querySelector('#message-form').addEventListener('submit', (e)=>{
     e.preventDefault()
-    const message = document.querySelector('input').value
+    const message = e.target.elements.message.value //document.querySelector('input').value  
     socket.emit('sendMessage', message)
 })
 socket.on('message', (message)=>{
